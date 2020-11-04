059c0fbefa9ce9b12fc7831e7976daeb035500bc
Abiodun Giwa <biodun1230@gmail.com>
2020-11-03 22:49:14 -0500

    week01 workshop, done!

--- src/main/java/com/codedifferently/part01/Greetings.java
+++ src/main/java/com/codedifferently/part01/Greetings.java
@@ -1,7 +1,16 @@
 package com.codedifferently.part01;
 
+import java.util.Scanner;
+
 public class Greetings {
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = input.next();
+        if (name.equalsIgnoreCase("Hakim") || name.equalsIgnoreCase("Rasheeda")){
+            System.out.println("Welcome to Java " + name);
+        } else {
+            System.out.println("You do not belong here");
+        }
     }
 }
--- src/main/java/com/codedifferently/part02/TooHighTooLow.java
+++ src/main/java/com/codedifferently/part02/TooHighTooLow.java
@@ -1,8 +1,29 @@
 package com.codedifferently.part02;
 
+import java.util.Scanner;
+
 public class TooHighTooLow {
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Integer mysteryNumber = 5;
+        System.out.println("Guess a number between 1 and 10: ");
+        Integer guessedNumber = input.nextInt();
+        Integer counter = 0;
+        do {
+            if (guessedNumber < mysteryNumber) {
+                System.out.println("Your guess is too low, try again.");
+                counter++;
+                guessedNumber = input.nextInt();
+            }
+            if (guessedNumber > mysteryNumber) {
+                System.out.println("Your guess is too high, try again");
+                counter++;
+                guessedNumber = input.nextInt();
+            }
+        } while (guessedNumber != mysteryNumber);
 
+            System.out.println("That's the correct guess, after " + counter + " tries");
     }
+
 }
